19a4e7eaab74fce77646585d5e7af7fc8757e21c
daldo <kimdohyunlll@naver.com>
2019-08-12 23:45:14 +0900

    Using self DI(Strong cohesion)    1. No singleton

--- TobySpring/src/user/dao/UserDAO.java
+++ TobySpring/src/user/dao/UserDAO.java
@@ -16,10 +16,9 @@ public class UserDAO {
 		private JdbcContext jdbcContext;
 		private DataSource dataSource;
 
-		public void setJdbcContext(JdbcContext jdbcContext) {
-			this.jdbcContext = jdbcContext;
-		}
 		public void setDataSource(DataSource dataSource) {
+			this.jdbcContext = new JdbcContext();
+			jdbcContext.setDataSource(dataSource);
 			this.dataSource = dataSource;
 		}
 

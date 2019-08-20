e41df07489775d4215944104a28a28be61981222
daldo <kimdohyunlll@naver.com>
2019-08-21 01:04:39 +0900

    Set Duplicate Exception using Spring DB error code mapping    1. SQLException to DataAccessException(Wrap, Runtime Exception)    2. DB error code mapping to DataAccessException SubClass

--- TobySpring/src/user/dao/UserDAO.java
+++ TobySpring/src/user/dao/UserDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -31,11 +32,11 @@ public class UserDAO {
 			this.jdbcTemplate = new JdbcTemplate(dataSource);
 		}
 
-		public void add(final User user) throws SQLException{
+		public void add(final User user) throws DuplicateKeyException{
 			this.jdbcTemplate.update("INSERT INTO USERS(ID, NAME, PASSWORD) VALUES(?,?,?)", user.getId(),user.getName(),user.getPassword());
 		}
 		
-		public User get(String id) throws SQLException{
+		public User get(String id) {
 			return this.jdbcTemplate.queryForObject("SELECT * FROM USERS WHERE ID = ?", 
 					new Object[] {id}, 
 					this.userMapper);
@@ -46,7 +47,7 @@ public class UserDAO {
 					this.userMapper);
 		}
 		
-		public void deleteAll() throws SQLException{
+		public void deleteAll() {
 			this.jdbcTemplate.update("DELETE FROM USERS");
 		}
 		

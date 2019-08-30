846493154a2d0a7d4550bc30f38d4d5085021324
daldo <kimdohyunlll@naver.com>
2019-08-30 22:45:10 +0900

    Refactoring transaction part    1. Using method extraction

--- TobySpring/src/user/service/UserService.java
+++ TobySpring/src/user/service/UserService.java
@@ -36,17 +36,20 @@ public class UserService {
 		TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
 		
 		try {
-			List<User> users = userDAO.getAll();
-			for(User user : users) {
-				if(canUpgradeLevel(user)) {
-					upgradeLevel(user);
-				}
-			}
-			
+			upgradeLevelsInternal();
 			this.transactionManager.commit(status);
 		}catch(Exception e) {
 			this.transactionManager.rollback(status);
 			throw e;
+		}
+	}
+	
+	private void upgradeLevelsInternal() {
+		List<User> users = userDAO.getAll();
+		for(User user : users) {
+			if(canUpgradeLevel(user)) {
+				upgradeLevel(user);
+			}
 		}
 	}
 	

7521bf36b20f89ce47c9c4a49bd1a1ca5735eb1b
daldo <kimdohyunlll@naver.com>
2019-09-04 00:19:56 +0900

    Apply the add-on only if it matches the method name

--- TobySpring/src/learningtest/jdk/UpperCaseHandler.java
+++ TobySpring/src/learningtest/jdk/UpperCaseHandler.java
@@ -14,7 +14,7 @@ public class UpperCaseHandler implements InvocationHandler {
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		// TODO Auto-generated method stub
 		Object ret = method.invoke(target, args);
-		if(ret instanceof String) {
+		if(ret instanceof String && method.getName().startsWith("say")) {
 			return ((String)ret).toUpperCase();	
 		}else {
 			return ret;

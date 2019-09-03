f32467ed9c06d1c79aca295c5bdec138ded03da3
daldo <kimdohyunlll@naver.com>
2019-09-04 00:17:48 +0900

    Using Object type for wide use    1. If return type is String, do toUpperCase()

--- TobySpring/src/learningtest/jdk/UpperCaseHandler.java
+++ TobySpring/src/learningtest/jdk/UpperCaseHandler.java
@@ -4,17 +4,21 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UpperCaseHandler implements InvocationHandler {
-	Hello target;
+	Object target;
 	
-	public UpperCaseHandler(Hello target){
+	public UpperCaseHandler(Object target){
 		this.target = target;
 	}
 	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		// TODO Auto-generated method stub
-		String ret = (String)method.invoke(target, args);
-		return ret.toUpperCase();
+		Object ret = method.invoke(target, args);
+		if(ret instanceof String) {
+			return ((String)ret).toUpperCase();	
+		}else {
+			return ret;
+		}
 	}
 
 }

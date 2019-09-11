e63f38063439d98657cd81b53e3e4de6dc9d7171
daldo <kimdohyunlll@naver.com>
2019-09-11 23:05:51 +0900

    Using AspectJ expression and execution for pointcut    1. Don't have to make another code. Just make one Bean    2. Study AspectJ expression

--- TobySpring/src/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ TobySpring/src/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -27,4 +27,30 @@ public class PointcutExpressionTest {
 				pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));
 		
 	}
+	
+	@Test
+	public void pointcut() throws Exception{
+		tagetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+		//Skip Other test method p.496
+	}
+	
+	public void tagetClassPointcutMatches(String expression, boolean... expected) throws Exception{
+		pointcutMatches(expression, expected[0], Target.class, "hello");
+		pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+		pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+		pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+		pointcutMatches(expression, expected[4], Target.class, "method");
+		pointcutMatches(expression, expected[5], Bean.class, "method");
+		
+	}
+	
+	public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws Exception{
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(expression);
+		
+		assertThat(pointcut.getClassFilter().matches(clazz) &&
+				pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null), is(expected));
+		
+		
+	}
 }
